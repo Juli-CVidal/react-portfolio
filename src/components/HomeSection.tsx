9b66f0ca4ac60392413632f32eb50b5b82d03cfa
Juli-CVidal <juli.vidal.019@gmail.com>
2023-08-12 12:31:42 -0300

    Improved components and styles

--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -6,7 +6,7 @@ const HomeSection = () => {
     <section className="home" id="home">
       <div className="hero">
         <img
-          className="profile__picture border__5 box__shadow"
+          className="profile__picture box__shadow radius__5"
           alt="Profile picture"
           src="/src/assets/imgs/profile__picture.webp"
         />
@@ -14,10 +14,8 @@ const HomeSection = () => {
           <h1 className="section__title">
             Hi, I'm Julián<span className="highlight__cursive">Vidal</span>
           </h1>
-          <div>
-            I'm a
-            <TextSlider />
-          </div>
+
+          <TextSlider speed={5000} texts={["FullStack Developer", "Quality Assurance"]}/>
         </div>
       </div>
     </section>
--- src/components/TextSlider.tsx
+++ src/components/TextSlider.tsx
@@ -1,36 +1,47 @@
 import "../assets/styles/text_slider.css";
 import { useState, useEffect } from "react";
 
-const TextSlider = () => {
-  const texts: string[] = ["FullStack Developer", "Quality Assurance"];
-  const [currentTextIndex, setCurrentTextIndex] = useState<number>(0);
+export type TextSliderProps = {
+  speed: number;
+  texts: string[];
+};
 
+const TextSlider = ({ speed, texts }: TextSliderProps) => {
+  const [currentTextIndex, setCurrentTextIndex] = useState(0);
+
+  const animationSpeed = `${speed}ms`;
   useEffect(() => {
+    const length = texts.length;
     const interval = setInterval(() => {
       setCurrentTextIndex(
-        (currentTextIndex) => (currentTextIndex + 1) % texts.length
+        (currentTextIndex) => (currentTextIndex + 1) % length
       );
-    }, 2500);
+    }, speed);
 
     return () => {
       clearInterval(interval);
     };
-  }, []);
+  }, [speed, texts]);
+
+  useEffect(() => {
+    document.documentElement.style.setProperty(
+      "--animation-speed",
+      animationSpeed
+    );
+  }, [animationSpeed]);
 
   return (
     <div className="text__slider">
-      {texts.map((text, index) => {
-        return (
-          <div
-            key={text}
-            className={
-              "text__slide " + (currentTextIndex === index ? "active" : "")
-            }
-          >
-            {text}
-          </div>
-        );
-      })}
+      {texts.map((text, index) => (
+        <p
+          key={index}
+          className={`text__slide ${
+            currentTextIndex === index ? "active" : ""
+          }`}
+        >
+          {text}
+        </p>
+      ))}
     </div>
   );
 };
